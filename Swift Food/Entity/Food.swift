627af3a7b61378da12ef961c3fbf9967bbe29838
nstosic <nemanjastosic@outlook.com>
2020-10-24 22:29:59 +0200

    Data model layer

---
+++ Swift Food/Entity/Food.swift
@@ -0,0 +1,16 @@
+//
+//  Food.swift
+//  Swift Food
+//
+//  Created by Nemanja Stosic on 18/10/2020.
+//
+
+import Foundation
+
+struct Food : BaseEntity {
+    let photoURL: String
+    let name: String
+    let description: String
+    let portion: String
+    let price: Double
+}
